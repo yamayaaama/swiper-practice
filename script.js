9cd9755985694554301031c1549f7c9a0c974090
yamayaaama <yama.yaaama512@gmail.com>
2024-12-16 05:31:13 +0900

    スライドは可能になったが、swiperのdocsをみたらやり方が違っていた

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 2,
+  spaceBetween: 24,
   pagination: {
     el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
