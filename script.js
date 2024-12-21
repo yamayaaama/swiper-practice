9828e4fbeabd72369e0436e7d3e07fe06dd17ac7
yamayaaama <yama.yaaama512@gmail.com>
2024-12-22 06:59:21 +0900

    一旦終了

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 24,
   pagination: {
     el: '.swiper-pagination',
@@ -11,4 +11,9 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    750: {
+      slidesPerView: 2,
+    }
+  }
 });
